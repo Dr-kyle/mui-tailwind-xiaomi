3d55f8e474546303f3feb6347ac490aa599c667f
Dr-kyle <13636814471@163.com>
2023-06-10 08:48:12 +0800

    init

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p className=''>小米字体</p>
+        <p className=''>小米字体</p>
+        <p className='font-misans-demibold'>正常字体</p>
+      </div>
+    </main>
+  )
+}
